713343f16c0b683d01c6ac6e839ec77494860cf7
iLexN <ilex.job@gmail.com>
2016-01-26 15:09:38 +0800

    add conversionJs method to helper

--- src/QuoteHelper.php
+++ src/QuoteHelper.php
@@ -17,7 +17,7 @@ class QuoteHelper
 <input type="hidden" value="{$_GET['adps']}" name="adps">
 END;
     }
-    
+
     public static function googleBuildQueryStr()
     {
         $ar = array('kwds', 'mtch', 'cmid', 'dgid', 'kwid', 'netw', 'dvce', 'crtv', 'adps');
@@ -27,9 +27,14 @@ END;
         }
         return http_build_query($out);
     }
-    
+
     public static function visitorJS($source)
     {
         return '<script type="text/javascript" src="//resources.pacificprime.com/widget/visitor.js?ref=PPI&tag='.$source.'"></script>';
+    }
+
+    public static function conversionJS($uid)
+    {
+        return '<script src="//resources.pacificprime.com/widget/conversion.js?uid='.$uid.'"></script>';
     }
 }
