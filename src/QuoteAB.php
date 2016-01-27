ed9a9f38b15a44fb7ac9296d31d3a45fc91a5863
iLexN <ilex.job@gmail.com>
2016-01-27 15:10:39 +0800

    add AB Test

---
+++ src/QuoteAB.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace PP\Common;
+
+class QuoteAB
+{
+    private $path;
+
+    private $key;
+
+    private $version = array();
+
+    /**
+     *
+     * @param type $path    json file path
+     * @param type $key  unique key
+     * @throws Exception
+     */
+    public function __construct($path, $version = array(), $key = 'langing')
+    {
+        $this->checkAll();
+
+        $this->path = $path;
+        $this->key = $key;
+        $this->version = $version;
+    }
+
+    public function getVersion()
+    {
+        if (!isset($_SESSION['abTest'][$this->key]) && $_SESSION['abTest'][$this->key] == '') {
+            $_SESSION['abTest'][$this->key] = $this->getAB($this->getJsonDate());
+        }
+        return $this->version[$_SESSION['abTest'][$this->key]];
+    }
+
+    private function getJsonDate()
+    {
+        $numJson = file_get_contents($this->path);
+
+        if (empty($numJson)) {
+            return array('A'=>0,'B'=>0);
+        }
+        return json_decode($numJson, true);
+    }
+
+    private function getAB($data = array())
+    {
+        $versionAB = '';
+        if ($data['A'] <= $data['B']) {
+            $data['A'] =  $data['A'] + 1 ;
+            $versionAB =  'A';
+        } else {
+            $data['B'] =  $data['B'] + 1 ;
+            $versionAB =  'B';
+        }
+        $this->writeJsonData($data);
+        return $versionAB;
+    }
+
+    private function writeJsonData($data)
+    {
+        file_put_contents($this->path, json_encode($data));
+    }
+
+    private function checkPath($path)
+    {
+        if (!is_writable($path)) {
+            throw new \Exception('Path not exist or not writeable : ' . $path);
+        }
+    }
+
+    private function checkData($version)
+    {
+        if (empty($version) || !array_key_exists('A', $version) || !array_key_exists('B', $version)) {
+            throw new \InvalidArgumentException('version data is wrong, please set it ["A"=>"versionA","B"=>"versionB"]');
+        }
+    }
+
+    private function checkSession(){
+        if (!isset($_SESSION)) {
+            throw new \RuntimeException('Session not found. Forget session_start() ?');
+        }
+    }
+
+    private function checkAll(){
+        $this->checkSession();
+        $this->checkPath($path);
+        $this->checkData($version);
+    }
+}
