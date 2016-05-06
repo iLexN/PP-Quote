7dcc29093ec8a242d67eb64d02d137c0bc64371d
iLexN <ilex.job@gmail.com>
2016-05-06 17:43:51 +0800

    remove static

--- src/QuoteHelper.php
+++ src/QuoteHelper.php
@@ -1,24 +1,39 @@
 <?php
 
-namespace PP\Common;
+namespace PP\Common\Quote;
 
 class QuoteHelper
 {
-    public static function googleAdInputField()
+    private $source;
+
+    private $ref;
+
+    public function __construct($ref , $source){
+        $this->source = $source;
+        $this->ref = $ref;
+    }
+    
+    public function googleAdInputField()
     {
+
+        $ar = array('kwds', 'mtch', 'cmid', 'dgid', 'kwid', 'netw', 'dvce', 'crtv', 'adps');
+        foreach ( $ar as $k ) {
+            $out[$k] = isset($_GET[$k]) ? $_GET[$k] : '';
+        }
+
         return <<<END
-<input type="hidden" value="{$_GET['mtch']} {$_GET['kwds']}" name="keywords">
-<input type="hidden" value="{$_GET['cmid']}" name="cmid">
-<input type="hidden" value="{$_GET['dgid']}" name="dgid">
-<input type="hidden" value="{$_GET['kwid']}" name="kwid">
-<input type="hidden" value="{$_GET['netw']}" name="netw">
-<input type="hidden" value="{$_GET['dvce']}" name="dvce">
-<input type="hidden" value="{$_GET['crtv']}" name="crtv">
-<input type="hidden" value="{$_GET['adps']}" name="adps">
+<input type="hidden" value="{$out['mtch']} {$out['kwds']}" name="keywords">
+<input type="hidden" value="{$out['cmid']}" name="cmid">
+<input type="hidden" value="{$out['dgid']}" name="dgid">
+<input type="hidden" value="{$out['kwid']}" name="kwid">
+<input type="hidden" value="{$out['netw']}" name="netw">
+<input type="hidden" value="{$out['dvce']}" name="dvce">
+<input type="hidden" value="{$out['crtv']}" name="crtv">
+<input type="hidden" value="{$out['adps']}" name="adps">
 END;
     }
 
-    public static function googleBuildQueryStr()
+    public function googleBuildQueryStr()
     {
         $ar = array('kwds', 'mtch', 'cmid', 'dgid', 'kwid', 'netw', 'dvce', 'crtv', 'adps');
         $out = array();
@@ -29,12 +44,12 @@ END;
         return http_build_query($out);
     }
 
-    public static function visitorJS($source)
+    public function visitorJS()
     {
-        return '<script type="text/javascript" src="//resources.pacificprime.com/widget/visitor.js?ref=PPI&tag='.$source.'"></script>';
+        return '<script type="text/javascript" src="//resources.pacificprime.com/widget/visitor.js?ref='.$this->ref.'&tag='.$this->source.'"></script>';
     }
 
-    public static function conversionJS($uid)
+    public function conversionJS($uid)
     {
         return '<script src="//resources.pacificprime.com/widget/conversion.js?uid='.$uid.'"></script>';
     }
